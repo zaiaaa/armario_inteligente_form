cdc1d578b5fa1cfcc700a46586e6d42b6c7ad433
zaiaaa <gustavozaia75@gmail.com>
2025-08-03 01:51:01 -0300

    retirando arquivos inuteis

--- src/components/formRetirada.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const formRetirada = () => {
-  return (
-    <div>formRetirada</div>
-  )
-}
-
-export default formRetirada
--- src/components/navBar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const navBar = () => {
-  return (
-    <div>navBar</div>
-  )
-}
-
-export default navBar
