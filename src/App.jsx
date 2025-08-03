e5104e2da937bfdc981e0cc2d006bb0f29730f80
zaiaaa <gustavozaia75@gmail.com>
2025-08-03 01:51:25 -0300

    instanciando rotas e o chakra UI.

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {Retirada} from './pages/Retirada/Retirada'
+import { RevisaoRetirada } from './pages/RevisaoRetirada/RevisaoRetirada'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Inter', system-ui, Avenir, Helvetica, Arial, sans-serif`,
+    body: `'Inter', system-ui, Avenir, Helvetica, Arial, sans-serif`,
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  },
+})
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <ChakraProvider theme={theme}>
+          <Routes>
+            <Route path='/retirada' element={<Retirada />}/>
+            <Route path='/retirada/revisao' element={<RevisaoRetirada />}/>
+          </Routes>
+        </ChakraProvider>
+      </Router>
+
     </>
   )
 }
