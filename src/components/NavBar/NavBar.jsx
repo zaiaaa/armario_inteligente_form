82af1aa8ed9f4c5837c45deee00f43996cbd2cfd
zaiaaa <gustavozaia75@gmail.com>
2025-08-03 01:54:04 -0300

    navbar estilizada e responsiva conforme o figma

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./navbar.css"
+import { Heading } from '@chakra-ui/react'
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      <img src="../src/assets/toyota_logo_nav.png" alt="" width={'100px'} />
+      <Heading as="h3" fontWeight="bold" fontSize="30px">
+        RETIRADA DE LOCKOUTS
+      </Heading>
+
+    </div>
+  )
+}
+
+export {NavBar}
