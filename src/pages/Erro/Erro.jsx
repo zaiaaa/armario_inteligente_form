1c7b06d60fd2188a71f9970043c89f04f1ea6cc4
zaiaaa <gustavozaia75@gmail.com>
2025-08-05 22:22:29 -0300

    pagina de erro após a revisão dos lockouts

---
+++ src/pages/Erro/Erro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./erro.css"
+import { NavBar } from '../../components/NavBar/NavBar'
+import erro from "../../assets/erro.png"
+
+const Erro = () => {
+  return (
+    <>
+        <NavBar />
+        <div className='content red'>
+            <img src={erro} width="320px" style={{marginTop: "30px"}} alt="" />
+            <p style={{color: "white", fontSize: "30px"}}>Dados não registrados!!</p>
+
+            <p style={{color: "white", fontSize: "30px"}}>Erro: {}</p>
+        </div>
+    </>
+)
+}
+
+export {Erro}
