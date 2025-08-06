99dc0510f40ca041332043a6dc658a3ee6dd40f1
zaiaaa <gustavozaia75@gmail.com>
2025-08-05 22:23:39 -0300

    deixando pagina de retirada dinâmica

--- src/pages/Retirada/Retirada.jsx
+++ src/pages/Retirada/Retirada.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {NavBar} from '../../components/NavBar/NavBar'
 import { useState } from 'react'
 import { FormRetirada } from '../../components/formRetirada/formRetirada'
@@ -6,13 +6,53 @@ import { RevisaoRetirada } from '../RevisaoRetirada/RevisaoRetirada'
 import "./retirada.css"
 import { Button } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
+import { Api } from '../../services/api'
 
 const Retirada = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
+
+  const [usuario, setUsuario] = useState({})
+  const [lockouts, setLockouts] = useState([])
+
 
   const irParaRevisao = (values) => {
-    navigate('/retirada/revisao', { state: { values } })
+    navigate('/retirada/revisao', { state: { values, usuario } })
   }
+
+  useEffect(() => {
+    const handleGetUsuario = async () => {
+            try {
+                const fetch = await Api.get('/status_abertura/status')
+                setUsuario(fetch.data)
+
+            } catch (e) {
+              console.log(e)
+            }
+        }
+      
+    const handleGetLockouts = async() => {
+      try {
+          const fetch = await Api.get('/status_abertura')
+          setLockouts(fetch.data)
+
+      } catch (e) {
+        console.log(e)
+      }
+
+    }
+      handleGetUsuario()
+      handleGetLockouts()
+    
+  }, [])
+
+  // useEffect(() => {
+  //   if (usuario && Object.keys(usuario).length === 0) {
+  //     navigate('/nao-autorizado')
+  //   }
+  // }, [usuario])
+
+
+  console.log(lockouts)
 
 
 
@@ -21,8 +61,8 @@ const Retirada = () => {
     <>
       <NavBar/>
       <div className="content">
-        <p>Olá <span className='colaborador'>João Pedro!</span> Seja bem-vindo ao sistema de retirada de lockouts!</p>
-        <FormRetirada aoRevisar={ irParaRevisao }/>
+        <p>Olá <span className='colaborador'>{usuario.nome} ({usuario.id_colaborador})</span> Seja bem-vindo ao sistema de retirada de lockouts!</p>
+        <FormRetirada aoRevisar={ irParaRevisao } lockouts={lockouts}/>
       </div>
     </>
   )
