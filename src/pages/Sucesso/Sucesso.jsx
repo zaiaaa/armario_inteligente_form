5533b20c65afe5aacdb9a4035d62db6aac8f06c6
zaiaaa <gustavozaia75@gmail.com>
2025-08-05 22:27:59 -0300

    tela de sucesso após revisao

---
+++ src/pages/Sucesso/Sucesso.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./sucesso.css"
+import { NavBar } from '../../components/NavBar/NavBar'
+import success from "../../assets/success.png"
+
+const Sucesso = () => {
+  return (
+    <>
+        <NavBar />
+        <div className='content green'>
+            <img src={success} width="400px" style={{marginTop: "30px"}} alt="" />
+            <p style={{color: "white", fontSize: "30px"}}>Dados registrados com sucesso e trava aberta!</p>
+        </div>
+    </>
+)
+}
+
+export {Sucesso}
