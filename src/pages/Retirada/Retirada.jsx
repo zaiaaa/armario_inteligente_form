bce01d3b3db83fa587de72cf54999a4258a464ed
zaiaaa <gustavozaia75@gmail.com>
2025-08-06 22:11:59 -0300

    arrumando mis um nome em maiusculo

--- src/pages/Retirada/Retirada.jsx
+++ src/pages/Retirada/Retirada.jsx
@@ -7,7 +7,7 @@ import "./retirada.css"
 import { Button } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { Api } from '../../services/api'
-import { Loading } from '../../components/Loading/loading'
+import { Loading } from '../../components/Loading/Loading'
 import { DotLottieReact } from '@lottiefiles/dotlottie-react'
 
 const Retirada = () => {
