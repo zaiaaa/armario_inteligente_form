70f59e855ad2feaa3c8eb28dce8cc43c24f3960a
zaiaaa <gustavozaia75@gmail.com>
2025-08-06 21:59:50 -0300

    deixando navbar dinamica conforme pagina

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import "./navbar.css"
 import { Heading } from '@chakra-ui/react'
 
-const NavBar = () => {
+const NavBar = ({nomePagina = ""}) => {
   return (
     <div className="navbar">
       <img src="../src/assets/toyota_logo_nav.png" alt="" width={'100px'} />
       <Heading as="h3" fontWeight="bold" fontSize="30px">
-        RETIRADA DE LOCKOUTS
+        {nomePagina} DE LOCKOUTS
       </Heading>
 
     </div>
