58fd8878f22ad4babd06b0ef52204026668838d0
zaiaaa <gustavozaia75@gmail.com>
2025-08-06 22:30:17 -0300

    mudando path da logo toyota

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import "./navbar.css"
 import { Heading } from '@chakra-ui/react'
+import toyota_logo from "../../assets/toyota_logo_nav.png"
 
 const NavBar = ({nomePagina = ""}) => {
   return (
     <div className="navbar">
-      <img src="../src/assets/toyota_logo_nav.png" alt="" width={'100px'} />
+      <img src={toyota_logo} alt="" width={'100px'} />
       <Heading as="h3" fontWeight="bold" fontSize="30px">
         {nomePagina} DE LOCKOUTS
       </Heading>
