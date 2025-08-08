e1a6eb012b3eec3c15b8ec0c056b3e4cd962a2d8
zaiaaa <gustavozaia75@gmail.com>
2025-08-08 11:25:59 -0300

    páginas necessárias para o andamento do projeto

---
+++ src/components/formCadastraUsuario/FormCadastraCracha.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./formCadastraCracha.css"
+
+const FormCadastraCracha = () => {
+  return (
+    <>
+    {/* aqui vai vir o form */}
+    </>
+  )
+}
+
+export default FormCadastraCracha
