5bc9a23df063f2f72b5518c5bcc433430311d92c
zaiaaa <gustavozaia75@gmail.com>
2025-10-30 09:23:08 -0300

    deixando a pagina organizada e importando carousel

--- src/pages/Sobre/Sobre.jsx
+++ src/pages/Sobre/Sobre.jsx
@@ -1,10 +1,16 @@
-import { Box, VStack, HStack, Text, Heading, Avatar, SimpleGrid } from '@chakra-ui/react';
+import { Box, VStack, HStack, Text, Heading, Avatar, SimpleGrid, Image } from '@chakra-ui/react';
 import { NavBar } from '../../components/NavBar/NavBar';
 import zaia from "../../assets/resp/zaia.jpeg"
 import joao from "../../assets/resp/joao.jpeg"
 import gabi from "../../assets/resp/gabi.jpeg"
 import alisson from "../../assets/resp/alisson.jpeg"
+import bg_armario from "../../assets/bg-armario.jpeg"
+import furo from "../../assets/furo.jpeg"
+import trava from "../../assets/trava.jpeg"
 
+
+import { Carrossel } from '../../components/Carousel/Carousel';
+import {Footer} from "../../components/Footer/Footer"
 
 const Sobre = () => {
   const integrantes = [
@@ -19,40 +25,49 @@ const Sobre = () => {
     <>
     
         <NavBar nomePagina='SISTEMA'/>
-        <Box p={{ base: 4, md: 16 }} bg="gray.50" minH="100vh" className='content'>
+        
+        <Box className='content'>
+        <Carrossel imagens={[{ src: bg_armario, legenda: "Controle inteligente de lockouts" },
+        { src: furo, legenda: "Instalação complexa" },
+        { src: trava, legenda: "Bloqueio seguro" },
+        ]}/>
+        {/* <Image src={bg_armario}></Image> */}
         {/* Título */}
-        <VStack spacing={4} mb={10} textAlign="center">
-            <Heading as="h1" size="xl">Sobre o Projeto</Heading>
-            <Text fontSize="lg" maxW="700px">
+        
+        <VStack spacing={4} mb={10} align="center">
+            <Heading as="h1" fontSize={"35px"} size="xl" color={"red"}>Além da segurança</Heading>
+            
+            <Text textAlign={"justify"}>
             O projeto <strong>Armário Inteligente</strong> foi desenvolvido para otimizar o controle e a devolução de lockouts dentro da Toyota, garantindo maior segurança e praticidade para os colaboradores.
             </Text>
         </VStack>
 
+
         {/* Sobre Toyota e SENAI */}
-        <VStack spacing={4} mb={10} align="flex-start">
-            <Heading as="h2" size="lg">Toyota</Heading>
-            <Text>
+        <VStack spacing={4} mb={10} align="center">
+            <Heading as="h2" fontSize={"35px"} size="xl" color={"red"}>TOYOTA</Heading>
+            <Text textAlign={"justify"}>
             A Toyota é uma das líderes mundiais na indústria automotiva, com foco em qualidade, inovação e eficiência. Este projeto visa melhorar os processos internos da empresa utilizando tecnologia de automação.
             </Text>
 
-            <Heading as="h2" size="lg" mt={6}>SENAI</Heading>
-            <Text>
+            <Heading as="h2" size="lg" mt={6} color={"red"} fontSize={"35px"}>SENAI</Heading>
+            <Text textAlign={"justify"}>
             O SENAI apoia a formação técnica e profissional, incentivando projetos que unem teoria e prática. Nosso projeto foi desenvolvido com base nos conhecimentos adquiridos durante a formação no SENAI.
             </Text>
         </VStack>
 
         {/* Motivação do Projeto */}
-        <VStack spacing={4} mb={10} align="flex-start">
-            <Heading as="h2" size="lg">Por que criamos este projeto?</Heading>
-            <Text>
+        <VStack spacing={4} mb={10} align="center">
+            <Heading as="h2" size="lg" fontSize={"35px"} color={"red"}>Objetivo</Heading>
+            <Text textAlign={"justify"}>
             A criação do Armário Inteligente surgiu da necessidade de controlar de forma eficiente os lockouts e chaves dentro da Toyota. Nosso objetivo é aumentar a segurança, reduzir erros humanos e automatizar processos manuais.
             </Text>
         </VStack>
 
         {/* Integrantes */}
-        <VStack spacing={4} align="flex-start">
-            <Heading as="h2" size="lg">Integrantes do Time</Heading>
-            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={6} mt={4}>
+        <VStack spacing={4} align="center">
+            <Heading as="h2" size="lg" color={"red"}>Integrantes do Time</Heading>
+            <SimpleGrid columns={{ base: 1, md: 4 }} spacing={6} mt={4} mb={4}>
             {integrantes.map((pessoa, i) => (
                 <Box key={i} p={4} bg="white" borderRadius="md" shadow="md" textAlign="center">
                 <Avatar name={pessoa.nome} src={pessoa.foto} size="xl" mb={2} />
@@ -62,6 +77,8 @@ const Sobre = () => {
             ))}
             </SimpleGrid>
         </VStack>
+
+        <Footer />
         </Box>
     </>
   );
