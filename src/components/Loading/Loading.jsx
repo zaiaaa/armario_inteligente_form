a42c348af41941d68c2ba312e769855fa431ead1
zaiaaa <gustavozaia75@gmail.com>
2025-11-04 09:28:10 -0300

    tirando o dotlottie do loading -> ocupava 850ms

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,26 +1,15 @@
-import { Suspense, lazy } from 'react';
-import { Spinner } from '@chakra-ui/react'; // ou qualquer outro fallback leve
-
-// Lazy import do Lottie
-const DotLottieReact = lazy(() =>
-  import('@lottiefiles/dotlottie-react').then(mod => ({ default: mod.DotLottieReact }))
-);
+import { Spinner, Center } from '@chakra-ui/react';
 
 const Loading = () => {
   return (
-    <div style={{ marginTop: '5rem', display: 'flex', justifyContent: 'center' }}>
-      <Suspense
-        fallback={<Spinner size="xl" color="red.500" thickness="4px" speed="0.7s" />}
-      >
-        <DotLottieReact
-          src="/animations/loading.json"
-          loop
-          autoplay
-          style={{ width: 180, height: 180 }}
-          fetchpriority="low"
-        />
-      </Suspense>
-    </div>
+    <Center mt="5rem">
+      <Spinner
+        size="xl"         // Tamanho grande
+        thickness="4px"   // Espessura da borda
+        speed="0.7s"      // Velocidade de rotação
+        color="red.500"   // Cor vermelha
+      />
+    </Center>
   );
 };
 
